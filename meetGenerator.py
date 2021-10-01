a95ae6cb233c0d0cee940190ac2ee78594264fa6
Juan Sebastián <89955811+juansebastian2006@users.noreply.github.com>
2021-10-01 15:45:01 -0300

    Update meetGenerator.py

--- meetGenerator.py
+++ meetGenerator.py
@@ -1,22 +1,9 @@
-
 import random
 import string as st
 
 
-class Color:
-    HEADER = '\033[95m'
-    OK_BLUE = '\033[94m'
-    OK_CYAN = '\033[96m'
-    OK_GREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    END_C = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
 class Generator:
-    link_f = "https://meet.google.com/"
+    lf = "https://meet.google.com/"
     message = "Limit: 99"
 
     def generate(self, times):
@@ -24,11 +11,16 @@ class Generator:
 
         if times <= 99:
             for i in range(times):
-                links.append(Color.BOLD + Color.HEADER +
-                             f"[{str(i + 1).zfill(2)}] " + self.link_f +
-                             '-'.join(''.join(random.choices(st.ascii_lowercase, k=(4 if i == 1 else 3)))
-                                      for i in range(3))
-                             )
+                code = []
+                for x in range(3):
+                    code.append(''.join(
+                        random.choices(st.ascii_lowercase, k=(4 if x == 1 else 3))
+                    ))
+                links.append(self.lf + '-'.join(code))
 
             return "\n".join(links)
-        return Color.BOLD + Color.WARNING + self.message + Color.END_C
+        return self.message
+        
+gen = Generator()
+print(gen.generate(10))
+
